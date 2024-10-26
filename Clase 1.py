ccfcaf62a8f75eafe2afa5b9bfb673d9dc3d8d51
Wendy <wendylo151213@gmail.com>
2024-10-25 20:41:02 -0500

    first commit

---
+++ Clase 1.py
@@ -0,0 +1,11 @@
+MiPrimeraVariable = "Hola curso de python"
+
+print(MiPrimeraVariable)
+
+# Clase 2
+
+Var1 = 100
+var2 = 200
+
+varT = var1 + var2
+print = varT
---
+++ Clase 2.py
@@ -0,0 +1,16 @@
+# Ejercicio 1
+
+numero1 = float(input("Ingrese el primer numero: "))
+
+numero2 = float(input("Ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("Suma:", suma)
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicacion: {multiplicacion}")
+print(f"Division: {division}")
