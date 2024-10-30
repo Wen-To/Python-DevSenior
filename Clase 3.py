2fd8a1e970a5a0e293dca59029ae7574c6b8bf8d
Wen-To <wendylo151213@gmail.com>
2024-10-29 21:11:21 -0500

    Clase 1 - Python

---
+++ Clase 3.py
@@ -0,0 +1,12 @@
+#Class 3
+#Importar Librerias
+
+age = 18
+country = 'USA'
+
+if age >= 21 and country == 'USA':
+    print('You can buy alcohol')
+elif age >= 18 and country == 'COL':
+    print('You can buy alcohol')
+else
+    print()
